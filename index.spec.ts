c70a97a744a3a76b8c8aecc5206d156cda0e1722
adrocodes <adrocodes@users.noreply.github.com>
2023-06-04 13:09:09 +1000

    step two

--- index.spec.ts
+++ index.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest"
-import { AliveComponent, Survivor, SurvivorSystem, WoundComponent, WoundSystem } from ".";
+import { AliveComponent, EquipmentCapacitySystem, EquipmentComponent, EquipmentSystem, Survivor, SurvivorSystem, WoundComponent, WoundSystem } from ".";
 
 describe("Wounds", () => {
   test("Entity starts with 0 wounds", () => {
@@ -74,5 +74,41 @@ describe("Survivor", () => {
     SurvivorSystem.performAction(survivor);
 
     expect(survivor.actionsRemaining).toBe(0);
+  })
+})
+
+describe("Equipment", () => {
+  test("Can add/remove in hand equipment", () => {
+    const equip = new EquipmentComponent([], []);
+    equip.addInHand("Knife");
+    equip.addInHand("Pistol");
+    equip.removeInHand("Knife");
+
+    expect(equip.inHand).toEqual(["Pistol"]);
+  })
+
+  test("Can add/remove in reserve equipment", () => {
+    const equip = new EquipmentComponent([], []);
+    equip.addInReserve("Knife");
+    equip.addInReserve("Pistol");
+    equip.removeInReserve("Knife");
+
+    expect(equip.inReserve).toEqual(["Pistol"]);
+  })
+
+  test("Capacity is reduced when taking wounds", () => {
+    const survivor = new Survivor("Bob");
+    const equip = survivor.get(EquipmentComponent) as EquipmentComponent;
+
+    EquipmentSystem.equip(survivor, "Knife", "inReserve");
+    EquipmentSystem.equip(survivor, "Water Bottle", "inReserve");
+    EquipmentSystem.equip(survivor, "Frying Pan", "inReserve");
+
+    WoundSystem.update(survivor);
+
+    EquipmentCapacitySystem.update(survivor);
+
+    expect(equip.inReserveCapacity).toBe(2);
+    expect(equip.inReserve).toEqual(["Knife", "Water Bottle"]);
   })
 })
--- index.ts
+++ index.ts
@@ -49,6 +49,90 @@ export class AliveComponent implements Component {
   constructor(public alive: boolean) {}
 }
 
+export class EquipmentComponent implements Component {
+  public inHandCapacity = 2;
+  public inReserveCapacity = 3;
+
+  constructor(public inHand: string[], public inReserve: string[]) {}
+
+  public addInHand(item: string) {
+    if (this.inHand.length >= this.inHandCapacity) return;
+    this.inHand.push(item);
+  }
+
+  public removeInHand(item: string) {
+    this.inHand = this.inHand.filter(i => i !== item);
+  }
+
+  public addInReserve(item: string) {
+    if (this.inReserve.length >= this.inReserveCapacity) return;
+    this.inReserve.push(item);
+  }
+
+  public removeInReserve(item: string) {
+    this.inReserve = this.inReserve.filter(i => i !== item);
+  }
+}
+
+export class EquipmentSystem {
+  static equip(entity: Entity, item: string, hand: "inHand" | "inReserve") {
+    const equipment = entity.get(EquipmentComponent) as (EquipmentComponent | undefined);
+
+    if (!equipment) return;
+    
+    if (hand === "inHand") {
+      equipment.addInHand(item);
+    }
+
+    if (hand === "inReserve") {
+      equipment.addInReserve(item);
+    }
+  }
+
+  static unequip(entity: Entity, item: string, hand: "inHand" | "inReserve") {
+    const equipment = entity.get(EquipmentComponent) as (EquipmentComponent | undefined);
+
+    if (!equipment) return;
+    
+    if (hand === "inHand") {
+      equipment.removeInHand(item);
+    }
+
+    if (hand === "inReserve") {
+      equipment.removeInReserve(item);
+    }
+  }
+}
+
+export class EquipmentCapacitySystem {
+  static update(entity: Entity) {
+    const equipment = entity.get(EquipmentComponent) as (EquipmentComponent | undefined);
+    const wounds = entity.get(WoundComponent) as (WoundComponent | undefined);
+
+    if (!equipment || !wounds) return;
+    if (wounds.value === 0) return;
+
+    const inHandCapacity = equipment.inHandCapacity;
+    const inReserveCapacity = equipment.inReserveCapacity;
+
+    if (inReserveCapacity > 0) {
+      equipment.inReserveCapacity = Math.max(0, inReserveCapacity - wounds.value);
+
+      if (equipment.inReserve.length > equipment.inReserveCapacity) {
+        equipment.inReserve = equipment.inReserve.slice(0, equipment.inReserveCapacity);
+      }
+    }
+
+    if (inHandCapacity > 0) {
+      equipment.inHandCapacity = Math.max(0, inHandCapacity - wounds.value);
+
+      if (equipment.inHand.length > equipment.inHandCapacity) {
+        equipment.inHand = equipment.inHand.slice(0, equipment.inHandCapacity);
+      }
+    }
+  }
+}
+
 export class Survivor extends Entity {
   public static actionsPerTurn = 3;
   public actionsRemaining = Survivor.actionsPerTurn;
@@ -58,7 +142,8 @@ export class Survivor extends Entity {
     this
       .add(new NameComponent(name))
       .add(new WoundComponent(0, 2))
-      .add(new AliveComponent(true));
+      .add(new AliveComponent(true))
+      .add(new EquipmentComponent([], []));
   }
 
   get isAlive() {
